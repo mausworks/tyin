fe8595778186eaaabd35147cb13840b4efeb3e80
mausworks <rasmuswennerstrom@gmail.com>
2023-12-29 22:40:05 +0100

    feat: add throttle utility
    Similar to debounce, so it lives under util-

---
+++ src/util-throttle.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect, jest } from "bun:test";
+import throttle from "./util-throttle";
+
+test("throttle", async () => {
+  const callback = jest.fn();
+  const throttled = throttle(3, callback);
+
+  throttled();
+
+  expect(callback).toHaveBeenCalledTimes(1);
+
+  throttled();
+  throttled();
+  throttled();
+
+  await new Promise((resolve) => setTimeout(resolve, 3.01));
+
+  throttled();
+  throttled();
+  throttled();
+
+  expect(callback).toHaveBeenCalledTimes(2);
+
+  await new Promise((resolve) => setTimeout(resolve, 3.01));
+
+  expect(callback).toHaveBeenCalledTimes(3);
+});
