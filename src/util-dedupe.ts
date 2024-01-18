b648a0edb85d4543d7d6464c1ecc2e4af0827c3a
mausworks <rasmuswennerstrom@gmail.com>
2024-01-19 00:44:33 +0100

    fix: rename duration to cacheTime
    + improve docs

--- src/util-dedupe.ts
+++ src/util-dedupe.ts
@@ -5,7 +5,7 @@ export type DedupeOptions<T extends AsyncFunction> = {
    *
    * Defaults to `0`.
    */
-  duration?: number;
+  cacheTime?: number;
   /**
    * How long to ignore repeated calls for while the promise is pending.
    * Set to 0 to disable deduplication, and `Infinity` to deduplicate indefinitely.
@@ -15,7 +15,7 @@ export type DedupeOptions<T extends AsyncFunction> = {
   timeout?: number;
   /**
    * Hashes the arguments of the function to determine
-   * if it was called with the same arguments again.
+   * if it was called again with the same arguments again.
    *
    * Uses `JSON.stringify` by default.
    */
@@ -23,13 +23,21 @@ export type DedupeOptions<T extends AsyncFunction> = {
 };
 
 /** A function that returns a promise. */
-export type AsyncFunction<A = any> = (...args: A[]) => any;
+export type AsyncFunction<A = any> = (...args: A[]) => Promise<any>;
 
 /**
  * Returns a function that deduplicates (and optionally caches) the result of an async function
  * by comparing the arguments between calls.
- * @param fn The function which promises to remember.
+ * @param fn The function to remember promises for.
  * @param options (Optional) Configure the deduplication and caching.
+ * @example
+ * ```ts
+ * import dedupe from "tyin/util-dedupe";
+ *
+ * const fetchNote = dedupe((id: string) => fetch(`/api/note/${id}`), {
+ *   cacheTime: 5000
+ * });
+ * ```
  */
 export default function dedupe<T extends AsyncFunction<any>>(
   fn: T,
@@ -42,7 +50,7 @@ export default function dedupe<T extends AsyncFunction<any>>(
   const {
     hash = (...args) => JSON.stringify(args),
     timeout = Infinity,
-    duration: cacheDuration = 0,
+    cacheTime = 0,
   } = options;
 
   const clear = () => {
@@ -72,12 +80,10 @@ export default function dedupe<T extends AsyncFunction<any>>(
     oldHash = newHash;
 
     return (promise = fn(...args)
-      .finally(() => {
-        clearAfter(cacheDuration);
-      })
-      .catch((error: unknown) => {
+      .finally(() => clearAfter(cacheTime))
+      .catch((reason: unknown) => {
         clear();
-        return Promise.reject(error);
+        return Promise.reject(reason);
       }));
   }) as T;
 }
