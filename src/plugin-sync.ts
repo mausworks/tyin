1684c61cfa03e02716843dc674290c5848a85305
mausworks <rasmuswennerstrom@gmail.com>
2024-02-19 16:31:11 +0100

    fix: sync types
    This isn't perfect, but composing a mapped type onto itself is erratic.

--- src/plugin-sync.ts
+++ src/plugin-sync.ts
@@ -1,33 +1,40 @@
 import { Plugin } from "./extend";
 import { AnyState, StoreAPI } from "./store";
 import createCache from "./util-cache";
-import dedupe, { AsyncFunction } from "./util-dedupe";
+import dedupe from "./util-dedupe";
 
 /**
- * A function that pulls the state from an external source using some arguments.
+ * A user-defined function that pulls the state from an external source using some arguments.
  * The function should return a promise that resolves with the new state.
+ * @template T The type of the state.
  */
 export type PullFunction<T> = (...args: any[]) => Promise<T>;
 
 /**
- * A function that syncs the state with an external source, optionally using some extra arguments.
+ * A user-defined function that syncs the state with an external source, optionally using some extra arguments.
  * The function should return a promise that resolves when the sync is complete.
  * @template T The type of the state.
  */
 export type SyncFunction<T> = (state: T, ...extra: any[]) => Promise<any>;
 
-/** A sync function which doesn't include the state as the first argument. */
-export type CompiledSyncFunction<F extends SyncFunction<any>> = (
-  ...extra: ExtraArgs<F>
-) => ReturnType<F>;
+/** Returns the type of the extra parameters of a function. */
+export type ExtraParameters<T extends (first: any, ...args: any[]) => any> =
+  T extends (state: any, ...args: infer U) => any ? U : never;
 
-/** Returns the extra arguments of a sync function. */
-export type ExtraArgs<T> = T extends (state: any, ...args: infer U) => any
-  ? U
-  : never;
+/** A function that syncs the state. */
+export type CompiledSyncFunction<
+  T extends AnyState,
+  F extends SyncFunction<T>
+> = (...extra: ExtraParameters<F>) => ReturnType<F>;
+
+/** A function that resolves with a new state. */
+export type CompiledPullFunction<
+  T extends AnyState,
+  F extends PullFunction<T>
+> = (...args: Parameters<F>) => Promise<T>;
 
 /** Configures how to sync the state. */
-export type SyncOptions<T extends AnyState, F extends SyncFunction<T>> = {
+export type SyncOptions = {
   /**
    * The duration to cache the result of the sync function for, in milliseconds.
    *
@@ -45,91 +52,83 @@ export type SyncOptions<T extends AnyState, F extends SyncFunction<T>> = {
    *
    * Uses `JSON.stringify` by default.
    */
-  hash?: (state: T, ...extra: ExtraArgs<F>) => string | number;
+  hash?: (...args: any[]) => string | number;
 };
 
-/** Configure how to pull a new state into the store. */
-export type PullOptions<P extends PullFunction<any>> = {
-  /**
-   * The duration to cache the result of the pull function for, in milliseconds.
-   *
-   * Defaults to 0.
-   */
-  cacheTime?: number;
-  /**
-   * The duration to ignore repeated calls for while pulling is in progress, in milliseconds.
-   *
-   * Defaults to Infinity.
-   */
-  dedupeTimeout?: number;
-  /**
-   * Hash the arguments to determine whether they have changed since the last pull.
-   *
-   * Uses `JSON.stringify` by default.
-   */
-  hash?: (...args: Parameters<P>) => string | number;
-};
+/** Extracts all sync function keys from the sync setup */
+type FunctionKeys<T> = Exclude<Extract<keyof T, string>, `${string}Options`>;
 
-/** Extracts a record of the user-defined sync functions from a setup object. */
-export type SyncFunctions<T, S extends object> = {
-  [K in Extract<keyof S, string>]: K extends `pull${infer _}`
-    ? K extends `${infer _}Options`
-      ? never
-      : PullFunction<T>
+/** Ensures all the functions in a setup object are sync or pull functions. */
+export type FunctionSetup<T extends AnyState, S extends object> = {
+  [K in FunctionKeys<S>]: K extends `pull${string}`
+    ? PullFunction<T>
     : SyncFunction<T>;
 };
 
-/** Extracts the a record of the compiled sync function types from a setup object. */
-export type CompiledSyncFunctions<S extends object> = {
-  [K in keyof S as K extends `${infer _}Options`
-    ? never
-    : K]: S[K] extends AsyncFunction
-    ? K extends `pull${infer _}`
-      ? S[K]
-      : CompiledSyncFunction<S[K]>
+/** The suggested setup for the sync plugin. */
+export type SetupSuggestions<T extends AnyState> = {
+  /**
+   * Push the current state to an external source, optionally using some extra arguments.
+   *
+   * **Tip:** This is the suggested name for a sync function that pushes the state.
+   * You can define (and configure) additional sync functions with any name you like.
+   */
+  push?: SyncFunction<T>;
+  /** Configure the suggested push function. */
+  pushOptions?: SyncOptions;
+  /**
+   * Pull the state from an external source using some arguments.
+   *
+   * **Tip:** This is the suggested name for a pull function that fetches the state.
+   * You can define (and configure) additional pull functions as long as they start with "pull".
+   */
+  pull?: PullFunction<T>;
+  /** Configure the suggested pull function. */
+  pullOptions?: SyncOptions;
+  /**
+   * Delete the state from an external source, optionally using some extra arguments.
+   *
+   * **Tip:** This is the suggested name for a sync function that deletes the state.
+   * You can define (and configure) additional sync functions with any name you like.
+   */
+  delete?: SyncFunction<T>;
+  /** Configure the suggested delete function. */
+  deleteOptions?: SyncOptions;
+};
+
+/** Infers the options for each sync function from a setup object. */
+export type AvailableOptions<S extends object> = {
+  [K in FunctionKeys<S> as `${K}Options`]?: S[K] extends (...args: any) => any
+    ? SyncOptions
     : never;
 };
 
-/** The suggested sync functions. */
-export type SyncFunctionSuggestions<T extends AnyState = AnyState> = {
-  /** Define how to sync the state upstream. */
-  push?: SyncFunction<T>;
-  /** Define how to sync the state downstream. */
-  pull?: PullFunction<T>;
-  /** Define how to delete the state upstream. */
-  delete?: SyncFunction<T>;
+/** Extracts the a record of the compiled sync function types from a setup object. */
+export type Compile<T extends AnyState, S extends object> = {
+  [K in FunctionKeys<S>]: S[K] extends (...args: any) => any
+    ? K extends `pull${string}`
+      ? CompiledPullFunction<T, S[K]>
+      : CompiledSyncFunction<T, S[K]>
+    : never;
 };
 
 /** The API provided by the sync plugin. */
-export type SyncAPI<S extends object> = {
-  sync: CompiledSyncFunctions<S>;
+export type SyncAPI<T extends AnyState, S extends object> = {
+  sync: Compile<T, S>;
 };
 
-/** Extracts the options for the sync functions from a setup object. */
-export type SyncFunctionOptions<T extends AnyState, S extends object> = {
-  [K in Extract<keyof S, string> as K extends `${infer _}Options`
-    ? never
-    : `${K}Options`]: K extends `pull${infer _}`
-    ? S[K] extends PullFunction<T>
-      ? PullOptions<S[K]>
-      : never
-    : S[K] extends SyncFunction<T>
-    ? SyncOptions<T, S[K]>
-    : never;
-};
+/** Recomposes the keys of an intersection type into a single type */
+type Recompose<T> = { [K in keyof T]: T[K] } & {};
 
 /** Helper type to setup the sync plugin. */
-export type SyncSetup<T extends AnyState, R extends {}> = R &
-  (
-    | SyncFunctions<T, R>
-    | SyncFunctionOptions<T, R>
-    | SyncFunctionSuggestions<T>
-  );
+export type SyncSetup<T extends AnyState, S extends object> = Recompose<
+  S & FunctionSetup<T, S> & AvailableOptions<S>
+>;
 
 /** A plugin that allows you to sync the state upstream and downstream using a method of your choice. */
-export type SyncPlugin<T extends AnyState, R extends {}> = Plugin<
+export type SyncPlugin<T extends AnyState, S extends {}> = Plugin<
   StoreAPI<T>,
-  SyncAPI<R>
+  SyncAPI<T, S>
 >;
 
 /**
@@ -141,13 +140,13 @@ export type SyncPlugin<T extends AnyState, R extends {}> = Plugin<
  *
  * @example
  * ```ts
- * import storeHook from "tyin";
+ * import storeHook from "tyin/hook";
  * import sync from "tyin/plugin-sync";
  * import extend from "tyin/extend";
  * import { Note } from "@/types";
  *
  * const useUserNotes = extend(storeHook<Note[]>([]))
- *   .with(
+ *   .with((store) =>
  *     sync({
  *       push: (notes, userId: string) =>
  *         fetch(`/api/notes/${userId}`, {
@@ -162,9 +161,10 @@ export type SyncPlugin<T extends AnyState, R extends {}> = Plugin<
  *   .seal();
  * ```
  */
-export default function sync<T extends AnyState, S extends {}>(
-  setup: SyncSetup<T, S>
-): SyncPlugin<T, S> {
+export default function sync<
+  T extends AnyState,
+  S extends SetupSuggestions<T> | object
+>(setup: SyncSetup<T, S>): SyncPlugin<T, S> {
   const cache = createCache<Promise<any>>();
 
   return (store) => {
@@ -173,12 +173,12 @@ export default function sync<T extends AnyState, S extends {}>(
       {
         dedupeTimeout = Infinity,
         cacheTime = 0,
-        hash = (...args) => JSON.stringify(args),
-      }: SyncOptions<T, F> = {}
+        hash = JSONHash,
+      }: SyncOptions = {}
     ) => {
       const deduped = dedupe(fn, { timeout: dedupeTimeout });
 
-      return (...args: ExtraArgs<F>) => {
+      return (...args: ExtraParameters<F>) => {
         const state = store.get();
         const key = hash(state, ...args);
 
@@ -191,8 +191,8 @@ export default function sync<T extends AnyState, S extends {}>(
       {
         dedupeTimeout = Infinity,
         cacheTime = 0,
-        hash = (...args) => JSON.stringify(args),
-      }: PullOptions<F> = {}
+        hash = JSONHash,
+      }: SyncOptions = {}
     ) => {
       const deduped = dedupe(fn, { timeout: dedupeTimeout });
 
@@ -210,13 +210,15 @@ export default function sync<T extends AnyState, S extends {}>(
     for (const [name, fn] of Object.entries(setup)) {
       if (typeof fn !== "function") continue;
 
-      const options = (setup as any)[`${name}Options`];
+      const options = (setup as any)[name + "Options"];
 
       sync[name] = name.startsWith("pull")
-        ? cachedPull(fn, options || {})
-        : cachedSync(fn, options || {});
+        ? cachedPull(fn as any, options || {})
+        : cachedSync(fn as any, options || {});
     }
 
     return { sync };
   };
 }
+
+const JSONHash = (...args: any[]) => JSON.stringify(args);
